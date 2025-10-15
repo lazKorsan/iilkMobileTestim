06e0f76c7022be9395762e4165280f0eae6b262a
lazKorsan <ahmeteldes2002@gmail.com>
2025-10-15 06:13:32 +0300

    bir kaç önemsizi düzeltme

--- src/test/java/hesapMakinesiTesti.java
+++ src/test/java/hesapMakinesiTesti.java
@@ -12,28 +12,48 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
-public class HesapMak {
-    AndroidDriver<AndroidElement>driver;
 
-public class hesapMakinesiTesti {
 
-    AndroidDriver<AndroidElement> driver;
+    public class hesapMakinesiTesti {
 
-    @Test
-    public void TC_01(){
+        AndroidDriver<AndroidElement> driver;
 
-        DesiredCapabilities cap=new DesiredCapabilities();
-        cap.setCapability(MobileCapabilityType.DEVICE_NAME,"Pixel 4");
-        cap.setCapability(MobileCapabilityType.PLATFORM_NAME,"Android");
-        cap.setCapability(MobileCapabilityType.PLATFORM_VERSION,"10.0");
-        cap.setCapability(MobileCapabilityType.AUTOMATION_NAME,"UiAutomator2");
-        cap.setCapability(MobileCapabilityType.APP,"C:\\Users\\elifk\\IdeaProjects\\162_Appium\\Apps\\Calculator_8.4 (503542421)_Apkpure (3).apk");
+        @Test
+        public void TC_01() throws MalformedURLException {
+
+            DesiredCapabilities cap = new DesiredCapabilities();
+            cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel 2");
+            cap.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+            cap.setCapability(MobileCapabilityType.PLATFORM_VERSION, "16.0");
+            cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+            cap.setCapability(MobileCapabilityType.APP, "C:\\Users\\Hp\\IdeaProjects\\MobTests\\Apps\\Calculator_8.4 (503542421)_Apkpure (3).apk");
+
+
+            driver=new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"),cap);
+            driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+
+            Assert.assertTrue(driver.isAppInstalled("com.google.android.calculator"));
+
+            Assert.assertTrue(driver.isAppInstalled("com.google.android.calculator"));
+
+            // uygulamanin acildigini dogrular
+            Assert.assertTrue(driver.findElementById("com.google.android.calculator:id/op_sqrt").isDisplayed());
+
+            // 456 un 3 bölümünün 152 oldugunu hesap makinasindan dogrulayalim
+            driver.findElementByAccessibilityId("4").click();
+            driver.findElementByAccessibilityId("5").click();
+            driver.findElementById("com.google.android.calculator:id/digit_6").click();
+            driver.findElementByAccessibilityId("divide").click();
+            driver.findElementByAccessibilityId("3").click();
+
+            String sonuc =driver.findElementById("com.google.android.calculator:id/result_preview").getText();
+            int bekledigimSonuc=152;
+            Assert.assertEquals(Integer.parseInt(sonuc),bekledigimSonuc);
+
+
+        }
 
 
     }
 
-
-
-
-
-}
